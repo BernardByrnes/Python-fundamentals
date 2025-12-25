75e12bab48d0ee64d2258fcdfe77d48f698756e9
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-25 16:54:00 +0300

    update

--- functional_programming.py
+++ functional_programming.py
@@ -12,37 +12,74 @@
 
 # print(multiply_by2([1,2,3]))  
 
-#~ map(action, iterable)
-def multiply_by2(li):
-  return li*2
+#~ MAP(action, iterable)
+# def multiply_by2(li):
+#   return li*2
 
 
-print(map(multiply_by2, [1,2,3,4])) #this returns a map object not a list
+# print(map(multiply_by2, [1,2,3,4])) #this returns a map object not a list
 
-print(list(map(multiply_by2, [1,2,3,4])))
+# print(list(map(multiply_by2, [1,2,3,4])))
 
-#~Filter
+#~FILTER
 
-def check_odd(item):
-  if(item % 2 == 1):
-    return item
+# def check_odd(item):
+#   if(item % 2 == 1):
+#     return item
   
 
-print(list(filter(check_odd,[1,2,3,4,5,5,6,7,8,9])))
+# print(list(filter(check_odd,[1,2,3,4,5,5,6,7,8,9])))
 
-def has_a(name):
-  return "a" in name.lower()
+# def has_a(name):
+#   return "a" in name.lower()
 
-names = ["Alice", "Bob", "Sarah", "Mike", "Adam"]
+# names = ["Alice", "Bob", "Sarah", "Mike", "Adam"]
 
-result = list(filter(has_a, names))
+# result = list(filter(has_a, names))
 
-print(result)
+# print(result)
 
 #~ZIP
 
-my_list = [1,2,3]
-your_list = [10,20,30]
-names_of = ["arthur", "charles", "Drake"]
+# my_list = [1,2,3]
+# your_list = [10,20,30]
+# names_of = ["arthur", "charles", "Drake"]
 
-print(list(zip(my_list,your_list,names_of)))
+# print(list(zip(my_list,your_list,names_of)))
+
+# #~REDUCE
+# from functools import reduce
+
+# numbers = [1, 2, 3, 4]
+
+# def add(acc, item):
+#     return acc + item
+
+# result = reduce(add, numbers)
+
+# print(result)
+# # 
+my_list = [1,2,3]
+my_list10=[19,28,23,39,46,42,74,99,26,75,42,31]
+
+# def accumulator(acc,item):
+#   print(acc,item)
+#   return acc + item
+
+# print(reduce(accumulator,my_list,10))
+
+#~LAMBDA
+# used for functions u will use once
+
+# lambda param:action(param)
+from functools import reduce
+def multiply_2(item):
+  return item*2
+
+print(list(map(lambda item: item * 10, my_list)))
+print(list(filter(lambda item: item % 2 !=0, my_list10)))
+print(reduce(lambda acc, item: acc+item, my_list10)) 
+
+mixed_list = [(0,2),(4,3),(10,-1),(9,9)]
+mixed_list.sort(key=lambda x: x[1])
+print(mixed_list)
