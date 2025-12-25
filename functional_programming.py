dca5db50a807c1501425ef33696ceff2eab69476
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-25 16:04:57 +0300

    update

--- functional_programming.py
+++ functional_programming.py
@@ -20,3 +20,29 @@ def multiply_by2(li):
 print(map(multiply_by2, [1,2,3,4])) #this returns a map object not a list
 
 print(list(map(multiply_by2, [1,2,3,4])))
+
+#~Filter
+
+def check_odd(item):
+  if(item % 2 == 1):
+    return item
+  
+
+print(list(filter(check_odd,[1,2,3,4,5,5,6,7,8,9])))
+
+def has_a(name):
+  return "a" in name.lower()
+
+names = ["Alice", "Bob", "Sarah", "Mike", "Adam"]
+
+result = list(filter(has_a, names))
+
+print(result)
+
+#~ZIP
+
+my_list = [1,2,3]
+your_list = [10,20,30]
+names_of = ["arthur", "charles", "Drake"]
+
+print(list(zip(my_list,your_list,names_of)))
