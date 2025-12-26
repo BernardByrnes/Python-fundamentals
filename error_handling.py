23075df41c8c0b5b58d947815de6b4c34c74e320
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-27 00:31:45 +0300

    updated

--- error_handling.py
+++ error_handling.py
@@ -1,8 +1,29 @@
 #~eRRORS
 
-while True:
+# while True:
+#     try:
+#         age = int(input("what is your age?"))
+#         print(age)
+#     except ValueError:
+#         print('please enter a number')
+#     except ZeroDivisionError:
+#         print('please enter a number')
+#     else:
+#         print('thank you')
+#         break
+
+#Example 2
+
+# def sum(num1,num2):
+#     return num1 + num2
+
+# print(sum("1","2"))
+# =========================
+def sum(num1,num2):
   try:
-    age = int(input("what is your age?"))
-    print(age)
-  except:
-    print('please enter a number')
+    return num1 + num2
+  # except TypeError as err:
+  except (TypeError,ZeroDivisionError) as err:
+    print(f'please enter number {err}')
+
+print(sum(2,"2"))
