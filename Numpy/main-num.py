e61943d2d3a57d23f9449e831aeddfe829f2c0fb
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-29 21:34:55 +0300

    updated

--- Numpy/main-num.py
+++ Numpy/main-num.py
@@ -6,3 +6,32 @@ import numpy as np
 
 # print(my_list)
 
+# ==================================
+
+# array = np.array([1,2,3,4,])
+
+# array = array*10
+
+# print(array)
+
+# ====================================
+# array = np.array([[["A","B","C"],["D","E","F"],["G","H","I"]],
+#                   [["J","K","L"],["M","N","O"],["P","Q","R"]],
+#                   [["S","T","U"],["V","W","X"],["Y","Z","_"]]])
+
+# # print(array.ndim)
+# # print(array.shape)
+# # print(array[0,1,1])
+
+# word = array[0,0,2] + array[2,0,2] + array[1,2,0]
+# print(word)
+
+# =========================================================
+
+array = np.array([[1,2,3,4],
+                  [5,6,7,8],
+                  [9,10,11,12],
+                  [13,14,15,16]])
+
+# array[start:end:step]
+print(array[0])
