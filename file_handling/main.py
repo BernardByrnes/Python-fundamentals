6a80279eff46f7c66ea35dc2c9774c50c9c9c06d
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-29 19:20:17 +0300

    updated

--- file_handling/main.py
+++ file_handling/main.py
@@ -1,8 +1,44 @@
-txt_data = "i like Watermelon"
+# txt_data = "i like Watermelon"
 
-file_path = "output.txt"
+# file_path = "output.txt"
 
 
-with open(file=file_path, mode="w") as file:
-    file.write(txt_data)
-    print(f"txt file '{file_path}' was created")
+# with open(file=file_path, mode="w") as file:
+#     file.write(txt_data)
+#     print(f"txt file '{file_path}' was created")
+# =================================================================
+# txt_data = "i like Watermelon!"
+# txt_data2 = "i also like Oranges!"
+
+# file_path = "C:/Users/Ben/Desktop/gif/python-tutorials/output2.txt"
+
+# try:
+#     with open(file=file_path, mode="a") as file:
+#         file.write("\n" + txt_data2)
+#         print(f"txt file '{file_path}' was created")
+# except FileExistsError:
+#     print("That file already exists")
+
+# ======================================================================
+
+import json
+import csv
+# employees = ["Eugene", "Squidward", "Spongebob", "Patrick"]
+
+# employee = {
+#   "name": "Spongebob",
+#   "age": 30,
+#   "job": "cook"
+# }
+
+employees = [[],[],[],[],]
+
+file_path = "C:/Users/Ben/Desktop/gif/python-tutorials/output.json"
+
+try:
+    with open(file=file_path, mode="w") as file:
+        json.dump(employee,file, indent=4)
+          # file.write(employee + "\n")
+        print(f"json file '{file_path}' was created")
+except FileExistsError:
+    print("That file already exists")
