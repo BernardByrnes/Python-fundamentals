8445695b5d7605f5f23c934b5394039b05e5ce50
BernardByrnes <bernardtamboedu@gmail.com>
2026-01-09 23:34:13 +0300

    updated

--- Pandas/main.py
+++ Pandas/main.py
@@ -123,12 +123,12 @@ water_pokemon = df[df["Type1"]=="Water"]
 
 #=================================DATA CLEANING==============================
 # df = df.drop(columns=["Legendary", "No"])
-# df = df.dropna(subset=["Type2"])
-# df = df.fillna({"Type2": "None"})
+df = df.dropna(subset=["Type2"])
+df = df.fillna({"Type2": "None"})
 
-# df["Type1"] = df["Type1"].replace({"Grass": "GRASS", "Fire": "FIRE", "Water": "WATER"})
+df["Type1"] = df["Type1"].replace({"Grass": "GRASS", "Fire": "FIRE", "Water": "WATER"})
 
-# df["Name"] = df["Name"].str.lower()
-# df["Legendary"] = df["Legendary"].astype(bool)
+df["Name"] = df["Name"].str.lower()
+df["Legendary"] = df["Legendary"].astype(bool)
 
 print(df.to_string())
