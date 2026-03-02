7d5e34b94d24560d5811274cb33063658fe06a26
BernardByrnes <bernardtamboedu@gmail.com>
2026-03-02 23:09:53 +0300

    update

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ import random
 
 def get_computer_choice():
     return random.choice(["rock", "paper", "scissors"])
+    def get_winner(player, computer):
+    if player == computer:
+        return "tie"
 
 
 
